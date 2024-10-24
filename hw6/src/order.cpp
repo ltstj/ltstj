db3a53d70fa44968e6fa53f8ce5a5fd7d13a808d
Tj Kalapatapu <86987912+ltstj@users.noreply.github.com>
2024-10-23 21:31:47 -0400

    Delete hw6 directory

--- hw6/src/order.cpp
+++
@@ -1,8 +0,0 @@
-#include "order.hpp"
-
-Order::Order(const Vehicle& vehicle, const Customer& customer, const DeliveryOption& deliveryOption)
-    : vehicle(vehicle), customer(customer), deliveryOption(deliveryOption) {}
-
-std::string Order::getOrderDetails() const {
-    return "Order Details: \nVehicle: " + vehicle.getModel() + "\nCustomer: " + customer.getName() + "\nDelivery Option: " + deliveryOption.getType();
-}
